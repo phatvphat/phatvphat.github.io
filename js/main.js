21d6ed5b15bfb8caac066bd636dea8a2dc800c2a
phatvphat <phatvphat@gmail.com>
2024-05-09 13:55:37 +0700

    fix avatar, video autoplay (muted)

--- js/main.js
+++ js/main.js
@@ -326,7 +326,7 @@ $(function(){
     if (animation) {
 
       var text = $('#home .typer-title'),
-          textOne = "i'm a back-end developer",
+          textOne = "i'm a back-end (indie) developer",
           textTwo = "let's work together",
           textThree = "i can create awesome stuff";
 
